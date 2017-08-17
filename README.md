af57e68606046319c21dfe21557916fae378f457
jjhuang2017 <jhangjhan.huang@gmail.com>
2017-08-18 06:17:05 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# image-upload-system
+# 圖片上傳系統
+
+本系統目標為提供用戶上傳圖片並進行圖片裁剪(支援JPG、PNG)，可以將圖片剪裁結果以網址方式來表示不同尺寸的圖片。
+
+## 系統功能與技術說明
+
+* 前端系統：
+前端統採用React.js開發框架，提供高維護性與效能性的網頁介面。
+
+* 後端系統：
+後端系統採用node.js與Express來開發RESTful API服務，提供上傳圖片剪裁結果與回傳圖片縮址功能。(系統專案[image-processing-service](https://github.com/jjhuang2017/image-processing-service)
+
+## 系統安裝說明
+
+* step1：安裝[node.js](https://nodejs.org/en/)
+
+* step2：建立目錄並切換到建立完成的目錄
+```shell
+$ mkdir myapp
+$ cd myapp
+```
+* step3：安裝位於專案的套件(package.json)
+```shell
+$ npm install
+```
+
+* step4：啟動位於專案的程式
+```shell
+$ npm start
+```
+
+* step5：圖片上傳系統的網頁位址
+`http://localhost:3000`
---
+++ example/src/Demo.jsx
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import 'cropperjs/dist/cropper.css';
+import Cropper from '../../src/react-cropper';
+import $ from 'jquery';
+import 'cropper/dist/cropper.js';
+
+const src = 'img/Koala.jpg';
+
+export default class Demo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      src,
+      cropResult: null
+    };
+    this.cropImage = this.cropImage.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.uploadImage = this.uploadImage.bind(this);
+  }
+
+  onChange(e) {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.setState({ src: reader.result });
+    };
+    reader.readAsDataURL(files[0]);
+  }
+
+  cropImage() {
+    if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
+      return;
+    }
+    this.setState({
+      cropResult: this.cropper.getCroppedCanvas().toDataURL(),
+    });
+  }
+
+  uploadImage(){
+    var formData = new FormData();
+    //console.log(this.cropper.getCroppedCanvas().toDataURL());
+    formData.append('imageupload', this.cropper.getCroppedCanvas().toBlob());
+
+    $.ajax({
+		    type: 'POST',
+        url: 'http://localhost:3003/Image-Upload-Service',      
+        data: formData,
+		    crossDomain: true,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(){
+        }
+    });
+  }
+
+  getLargeImageURL(){
+    $(".image_url").empty();
+    $.ajax({
+		  type: 'GET',
+      url: 'http://localhost:3003/Large-Image-URL',    
+		  async: true,		
+	  	crossDomain: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(resp){   
+			  $(".image_url").html(resp);
+		  },
+	  	error: function(e){  
+			    
+      }  
+    });
+  }
+
+  getMediumImageURL(){
+    $(".image_url").empty();
+    $.ajax({
+		  type: 'GET',
+      url: 'http://localhost:3003/Medium-Image-URL',    
+		  async: true,		
+	  	crossDomain: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(resp){   
+			  $(".image_url").html(resp);
+		  },
+	  	error: function(e){  
+			    
+      }  
+    });
+  }
+
+  getSmallImageURL(){
+    $(".image_url").empty();
+    $.ajax({
+		  type: 'GET',
+      url: 'http://localhost:3003/Small-Image-URL',    
+		  async: true,		
+	  	crossDomain: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(resp){   
+			  $(".image_url").html(resp);
+		  },
+	  	error: function(e){  
+			    
+      }  
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="box" style={{ width: '50%', float: 'left' }}>
+          <h2><span>Step 1. 選擇圖片進行裁剪</span></h2>
+          <div>
+            <input type="file" onChange={this.onChange} style={{float: 'left' }}/>
+          </div>
+          <div>
+            <button onClick={this.cropImage} style={{ float: 'left' }}>裁剪圖片</button>
+          </div>
+          <br/>
+          <br/>
+          <Cropper
+            style={{ height: 400, width: '100%' }}
+            aspectRatio={16 / 9}
+            preview=".img-preview"
+            guides={false}
+            src={this.state.src}
+            ref={cropper => { this.cropper = cropper; }}
+          />
+        </div>
+
+        <div>
+          <div className="box" style={{ width: '50%', float: 'right' }}>
+            <h2><span>Step 2. 上傳圖片</span></h2>
+            <button onClick={this.uploadImage} style={{ float: 'left' }}>上傳圖片</button>
+            <br/>
+            <br/>
+            <img style={{ height: 400, width: '100%' }} src={this.state.cropResult} />
+          </div>
+        </div>
+
+        <div>
+          <div className="box" style={{ width: '100%', float: 'right' }}>
+            <h2><span>Step 3. 取得圖片縮址</span></h2>
+            <button onClick={this.getLargeImageURL} style={{ float: 'left' }}>大圖示 (Large)</button>
+            <button onClick={this.getMediumImageURL} style={{ float: 'left' }}>中圖示 (Medium)</button>
+            <button onClick={this.getSmallImageURL} style={{ float: 'left' }}>小圖示 (Small)</button>
+            <br/>
+            <br/>
+            <div className="image_url"></div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
